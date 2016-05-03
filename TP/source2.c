9d6ddd53d5679a66c87b3f022d5b37ca7cff6fe1
il190543 <il190543@iut21.u-bourgogne.fr>
2016-05-03 15:28:31 +0200

    Q8
    Mise en place du chronometre avec les secondes qui s'incrémente tout
    seules. (Avec un tempo de 1ms)

--- TP/source2.c
+++ TP/source2.c
@@ -11,12 +11,13 @@ byte *portc = 0xf82 ;
 int i=0;
 int nb=0;
 int seconde = 0;
-void affiche_7seg(byte);
-void affiche_7seg(byte val)
+void affiche_7seg(byte, byte);
+void affiche_7seg(byte val, byte num_affiche)
 {
 byte
 tab_7seg[]={0b00111111,0b00000110,0b01011011,0b01001111,0b01100110,0b01101101,0b01111101,0b00000111,0b01111111,0b01101111};
 *portb=tab_7seg[val];
+*portd=num_affiche;
 }
 void it_int0()
 {}
@@ -26,17 +27,17 @@ void it_int1()
 void it_tmr0()
 {
 i++;
-affiche_7seg(seconde % 10);
+affiche_7seg(seconde % 10, 0xfc);
 if ((i % 10) == 0) seconde ++;
 }
 void main(void)
 {
+init_it_tmr0();
 *trisc = 0xff;
 *trisd = 0 ;
 *trisb = 0;
 *portb = 0;
-*portd = 0xfe;
-init_it_tmr0();
+
 while(1){
 }
 }
